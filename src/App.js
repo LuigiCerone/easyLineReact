91f7f00af792bcba612b0aa4536446737ed750df
StefanoMartella <stefano.martella9614@gmail.com>
2019-03-08 15:42:18 +0100

    Drawer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {createStackNavigator, createAppContainer} from "react-navigation";
-import {Text, View} from 'react-native';
-import {SideMenu, List} from "react-native-elements";
+import {Text, View, Button} from 'react-native';
 import DepartmentListScreen from "./screens/DepartmentListScreen";
 import CourseListScreen from "./screens/CourseListScreen";
 import ActionListScreen from "./screens/ActionListScreen";
+import Drawer from 'react-native-drawer';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const AppNavigator = createStackNavigator({
     DepartmentListScreen: DepartmentListScreen,
@@ -15,38 +16,54 @@ const AppNavigator = createStackNavigator({
     defaultNavigationOptions: {
         headerStyle: {
             backgroundColor: 'orange'
-        }
+        },
+        headerLeft: (
+            <Button
+                icon={
+                    <Icon
+                        name="arrow-right"
+                        size={15}
+                        color="white"
+                    />
+                }
+                onPress={() => openControlPanel}
+                title=""
+            />
+        )
     }
 });
 
 const AppContainer = createAppContainer(AppNavigator);
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            isOpen: false
-        };
-        this.toggleSideMenu = this.toggleSideMenu.bind(this)
-    }
 
-    toggleSideMenu() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
+    closeControlPanel = () => {
+        this._drawer.close()
+    };
+    openControlPanel = () => {
+        this._drawer.open()
+    };
 
-    render() {
-        const MenuComponent = (
-            <View style={{flex: 1, backgroundColor: '#ededed', paddingTop: 50}}>
-
-            </View>
-        );
-
+    render () {
         return (
-            <AppContainer/>
+            <Drawer
+                type="static"
+                tapToClose={true}
+                openDrawerOffset={100}
+                styles={drawerStyles}
+                tweenHandler={Drawer.tweenPresets.parallax}
+                ref={(ref) => this._drawer = ref}
+                content={<Text>Qui mettere il componente Menu al posto di Text</Text>}
+            >
+                <AppContainer />
+            </Drawer>
         )
     }
+}
+
+const drawerStyles = {
+    drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 30},
+    main: {paddingLeft: 3},
 }
 
 export default App;
