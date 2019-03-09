05749527e9da36e209fd442b5f7990dd49bfc00b
StefanoMartella <stefano.martella9614@gmail.com>
2019-03-09 11:36:25 +0100

    Drawer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {createStackNavigator, createAppContainer} from "react-navigation";
-import {Text, View, Button} from 'react-native';
+import { createStackNavigator, createAppContainer, createDrawerNavigator } from "react-navigation";
+import { Button, Icon } from 'react-native-elements';
 import DepartmentListScreen from "./screens/DepartmentListScreen";
 import CourseListScreen from "./screens/CourseListScreen";
 import ActionListScreen from "./screens/ActionListScreen";
 import OfficeScreen from "./screens/OfficeScreen";
-import Drawer from 'react-native-drawer';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { Header } from "react-native-elements";
+//import Icon from 'react-native-vector-icons/FontAwesome';
 
 const AppNavigator = createStackNavigator({
     DepartmentListScreen: DepartmentListScreen,
@@ -14,58 +14,39 @@ const AppNavigator = createStackNavigator({
     ActionListScreen: ActionListScreen,
     OfficeScreen: OfficeScreen
 }, {
-    initialRouteName: "DepartmentListScreen",
     defaultNavigationOptions: {
-        headerStyle: {
-            backgroundColor: 'orange'
-        },
-        headerLeft: (
-            <Button
-                icon={
-                    <Icon
-                        name="arrow-right"
-                        size={15}
-                        color="white"
+        header: (
+            <Header
+                leftComponent={(
+                    <Button
+                        onPress={() => console.log('Pressed')}
+                        buttonStyle={{
+                            backgroundColor: '#822627'
+                        }}
+                        icon={
+                            <Icon
+                                name="menu"
+                                size={20}
+                                color="white"
+                            />
+                        }
                     />
-                }
-                onPress={() => App.openControlPanel}
-                title=""
-            />
-        )
-    }
+                )}
+                centerComponent={{ text: 'EasyLine', style: { color: '#fff' } }}
+                containerStyle={{
+                    backgroundColor: '#822627'
+                }}
+            />)
+    },
+    initialRouteName: "DepartmentListScreen"
 });
 
-const AppContainer = createAppContainer(AppNavigator);
+const drawerScreens = createDrawerNavigator({
+    Homepage:  AppNavigator
+}, {
+    initialRouteName: 'Homepage'
+});
 
-class App extends React.Component {
-
-    static closeControlPanel = () => {
-        this._drawer.close()
-    };
-    static openControlPanel = () => {
-        this._drawer.open()
-    };
-
-    render() {
-        return (
-            <Drawer
-                type="static"
-                tapToClose={true}
-                openDrawerOffset={100}
-                styles={drawerStyles}
-                tweenHandler={Drawer.tweenPresets.parallax}
-                ref={(ref) => this._drawer = ref}
-                content={<Text>Qui mettere il componente Menu al posto di Text</Text>}
-            >
-                <AppContainer/>
-            </Drawer>
-        )
-    }
-}
-
-const drawerStyles = {
-    drawer: {shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 30},
-    main: {paddingLeft: 3},
-}
+const App = createAppContainer(drawerScreens);
 
 export default App;
