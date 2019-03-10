7d398baa372b90cb9aa36234e194e1783f3a5048
D-hash <andreadasc96@gmail.com>
2019-03-10 12:46:55 +0100

    Table office

--- src/components/Office.js
+++ src/components/Office.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Table, TableWrapper, Row, Rows, Col } from 'react-native-table-component';
 import { withNavigation } from 'react-navigation';
 
 import { Colors } from '../constants';
@@ -7,12 +8,25 @@ import { Colors } from '../constants';
 class Office extends Component {
     render() {
         const { office } = this.props;
-
+        let tableHead = [''];
+        let tableDataStart = [];
+        for (let i = 0; i < office.timetable.length; i++)
+            {
+            tableHead = tableHead.concat(office.timetable[i].weekday);
+            //tableDataStart = tableDataStart.concat(office.timetable[i].start);
+            }
+        let tableTitle = ['Apertura'];
         return (
             <View>
                 <Text>{office.name}</Text>
-                <Text>{office.address}</Text>
-             </View>
+                <Table>
+                    <Row data={tableHead} flexArr={[1, 1, 2, 1, 1, 1]} />
+                    <TableWrapper style={styles.wrapper}>
+                        <Col data={tableTitle} style={styles.title} heightArr={[28, 28]} textStyle={styles.text} />
+                        <Rows data={tableDataStart} style={styles.row} textStyle={styles.text} />
+                    </TableWrapper>
+                </Table>
+            </View>
         );
     }
 }
@@ -24,6 +38,13 @@ class Office extends Component {
 //     </View>
 // }
 const styles = {
+    container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+    head: {  height: 40,  backgroundColor: '#f1f8ff'  },
+    wrapper: { flexDirection: 'row' },
+    title: { flex: 1, backgroundColor: '#f6f8fa' },
+    row: {  height: 28  },
+    text: { textAlign: 'center' },
+
     containerStyle: {
         borderColor: Colors.border,
         borderWidth: 1,
